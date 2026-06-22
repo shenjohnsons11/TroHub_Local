7deda0f715a1c4759c13fd3dc19f159a8fff744a
shenjohnsons11 <shenjohnsons11@gmail.com>
2026-06-22 18:15:51 +0700

    Update API to use localhost instead of server IP

--- constants/api.ts
+++ constants/api.ts
@@ -1,5 +1,5 @@
 import { Platform } from "react-native";
 
-const LOCAL_IP = Platform.OS === 'android' ? "10.0.2.2" : "192.168.1.12";
+const LOCAL_IP = Platform.OS === 'android' ? "10.0.2.2" : "localhost";
 
 export const API_BASE_URL = `http://${LOCAL_IP}:3000/api`;
