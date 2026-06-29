ec2d9e6b76baa25945ecdcda58904b6c27bacc62
kiet-it-22 <duongvykiet1234@gmail.com>
2026-06-30 01:56:01 +0700

    thêm iphone sài app

--- constants/api.ts
+++ constants/api.ts
@@ -1,5 +1,10 @@
 import { Platform } from "react-native";
 
-const LOCAL_IP = Platform.OS === 'android' ? "10.0.2.2" : "localhost";
+const LOCAL_IP =
+  Platform.OS === "android"
+    ? "10.0.2.2"
+    : Platform.OS === "ios"
+      ? "192.168.1.80"
+      : "localhost";
 
 export const API_BASE_URL = `http://${LOCAL_IP}:3000/api`;
