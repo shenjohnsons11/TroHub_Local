58920d4eae234bf6b8b0ccc6e4f3c8b7b72ee0b4
kiet-it-22 <duongvykiet1234@gmail.com>
2026-08-21 02:26:22 +0700

    thêm máy thật

--- constants/api.ts
+++ constants/api.ts
@@ -1,5 +1,5 @@
-import Constants from 'expo-constants';
 import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 
 const getHostIp = () => {
   const hostUri = Constants.expoConfig?.hostUri || Constants.expoGoConfig?.debuggerHost;
@@ -7,4 +7,7 @@ const getHostIp = () => {
   return Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
 };
 
-export const API_BASE_URL = `http://${getHostIp()}:5000/api`;
+const LOCAL_API_URL = `http://${getHostIp()}:5000/api`;
+
+export const API_BASE_URL =
+  process.env.EXPO_PUBLIC_API_BASE_URL || LOCAL_API_URL;
